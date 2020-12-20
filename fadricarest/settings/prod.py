db5b81c6a61068b40d8ed05c7332b3a11e8d5eb7
Jorge <jorge.gutierrezvila0@gmail.com>
2020-12-20 12:55:24 +0100

    Añadido email para el envío de errores en prod

--- fadricarest/settings/prod.py
+++ fadricarest/settings/prod.py
@@ -11,6 +11,21 @@ DATABASES = {
     "default": dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
+# Email SMTP sendgrid configuration
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = os.environ.get("SENDGRID_API_KEY")
+# Port 465 if a HTTPS connection is used
+# It's recommended in unencrypted connection use the port 587 "to avoid any rate limiting
+# that your server host may apply."
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+# Mailing with errors to ADMINS
+SERVER_EMAIL = DEFAULT_FROM_EMAIL
+ADMINS = [("Jorge", os.environ.get["ADMIN_EMAIL"])]
+
 # Secure options and Forcing Https connection
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
