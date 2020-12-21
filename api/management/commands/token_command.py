6948f791d2b028abd09d23d7c57da060b7817a15
Jorge <jorge.gutierrezvila0@gmail.com>
2020-12-21 18:24:07 +0100

    Creado Comando para generar token

---
+++ api/management/commands/token_command.py
@@ -0,0 +1,16 @@
+import uuid
+
+from django.core.management.base import BaseCommand, CommandError
+from api.models import Token
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        self.stdout.write(self.style.SUCCESS("CREANDO TOKEN"))
+        db_uuid = str(uuid.uuid4())
+        try:
+            Token.objects.create(token=db_uuid)
+        except django.db.utils.IntegrityError as e:
+            raise CommandError("¡ERROR! Token repetido, ejecute de nuevo el comando.")
+
+        self.stdout.write(self.style.SUCCESS("TOKEN CREADO CON ÉXITO: {}".format(db_uuid)))
