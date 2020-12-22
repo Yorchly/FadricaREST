79ea4498b8e300e553629fdaa7aff9595f5a0cde
Jorge <jorge.gutierrezvila0@gmail.com>
2020-12-22 21:27:38 +0100

    Views creadas

--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView
 
-# Create your views here.
+from api.models import TipoRoscon, Roscon
+from api.serializers import TipoRosconSerializer, RosconSerializer
+
+
+class TipoRosconListView(ListAPIView):
+    queryset = TipoRoscon.objects.all()
+    serializer_class = TipoRosconSerializer
+
+
+class RosconAllView(ListAPIView, CreateAPIView, UpdateAPIView):
+    queryset = Roscon.objects.all()
+    serializer_class = RosconSerializer
