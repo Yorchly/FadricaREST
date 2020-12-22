ef1cd85e52d343ddf2c3bea86010ca06ab905f71
Jorge <jorge.gutierrezvila0@gmail.com>
2020-12-22 23:46:58 +0100

    Check token añadido

--- api/views.py
+++ api/views.py
@@ -1,24 +1,31 @@
-from rest_framework.generics import UpdateAPIView
+from django.http import HttpResponseForbidden
 from rest_framework.mixins import ListModelMixin, CreateModelMixin, UpdateModelMixin
 from rest_framework.viewsets import GenericViewSet
 
 from api.models import TipoRoscon, Roscon
 from api.serializers import TipoRosconSerializer, RosconSerializer
+from api.utils import check_token
 
 
 class ListViewSet(ListModelMixin, GenericViewSet):
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
+    pass
 
 
 class ListCreateUpdateViewSet(ListModelMixin, UpdateModelMixin, CreateModelMixin, GenericViewSet):
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+    def dispatch(self, request, *args, **kwargs):
+        if check_token(self.request.GET.get("token", None)):
+            return super(ListCreateUpdateViewSet, self).dispatch(request, *args, **kwargs)
+        else:
+            return HttpResponseForbidden()
 
     def put(self, request, *args, **kwargs):
+        """
+        Si no se especifica la función put, no funciona correctamente el update
+        :param request:
+        :param args:
+        :param kwargs:
+        :return:
+        """
         return self.update(request, *args, **kwargs)
 
 
