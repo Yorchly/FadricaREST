45b8792c3ab90966a24d3194aa1682228b89a0cc
Jorge <jorge.gutierrezvila0@gmail.com>
2020-12-22 22:24:45 +0100

    Cambios en la Views (uso de los viewsets)

--- api/views.py
+++ api/views.py
@@ -1,14 +1,23 @@
-from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, UpdateModelMixin
+from rest_framework.viewsets import GenericViewSet
 
 from api.models import TipoRoscon, Roscon
 from api.serializers import TipoRosconSerializer, RosconSerializer
 
 
-class TipoRosconListView(ListAPIView):
+class ListViewSet(ListModelMixin, GenericViewSet):
+    pass
+
+
+class ListCreateUpdateViewSet(ListModelMixin, CreateModelMixin, UpdateModelMixin, GenericViewSet):
+    pass
+
+
+class TipoRosconListViewSet(ListViewSet):
     queryset = TipoRoscon.objects.all()
     serializer_class = TipoRosconSerializer
 
 
-class RosconAllView(ListAPIView, CreateAPIView, UpdateAPIView):
+class RosconAllView(ListCreateUpdateViewSet):
     queryset = Roscon.objects.all()
     serializer_class = RosconSerializer
